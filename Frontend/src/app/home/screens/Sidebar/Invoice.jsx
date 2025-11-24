1268a99d9f9c827bf423c7d7d8d8d4e8715b0048
sravanikaranam <karanamsravani12@gmail.com>
2025-11-24 11:31:22 +0530

    Billing screens completed

---
+++ Frontend/src/app/home/screens/Sidebar/Invoice.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+export default function InvoiceScreen({ navigation }) {
+  const router = useRouter();
+    return (
+    <View className="flex-1 bg-white mt-12">
+      
+      {/* Header */}
+      <View className="bg-[#22C55E] flex-row items-center justify-between px-4 py-4">
+        <TouchableOpacity onPress={() =>  router.push("/home/screens/Sidebar/Billing")}>
+          <Ionicons name="arrow-back" size={24} color="white" />
+        </TouchableOpacity>
+        <Text className="text-white text-xl font-bold">Invoice</Text>
+        <View style={{ width: 20 }} />
+      </View>
+
+      <ScrollView contentContainerStyle={{ padding: 20 }}>
+        
+        {/* Invoice Card */}
+        <View className="border border-gray-200 rounded-xl p-5 bg-white">
+
+          {/* Invoice Number */}
+          <Text className="text-gray-400 text-sm">Invoice No</Text>
+          <Text className="text-black font-bold mb-3">
+            F4CFC-77EB2-FCGA2-7F983
+          </Text>
+
+          {/* Date */}
+          <Text className="text-gray-400 text-sm">Date</Text>
+          <Text className="text-black font-semibold mb-3">
+            16/10/25
+          </Text>
+
+          {/* CURRENT PLAN LIKE YOUR SCREENSHOT */}
+         <Text className="text-[#22C55E] text-sm font-semibold mb-1 ml-24">
+  Current Plan
+</Text>
+
+
+          {/* Description */}
+          <Text className="text-gray-400 text-sm">Description</Text>
+          <Text className="text-black font-semibold mb-3">
+            Streaming Services
+          </Text>
+
+          {/* Service Period */}
+          <Text className="text-gray-500 text-xs">Service Period</Text>
+          <Text className="text-black font-semibold mb-3">
+            16/10/25 to 15/11/25
+          </Text>
+
+          {/* Amount */}
+          <Text className="text-gray-400 text-sm">Amount</Text>
+          <Text className="text-black font-bold mb-3">
+            $168.64
+          </Text>
+
+          {/* IGST */}
+          <Text className="text-gray-400 text-sm">IGST(18%)</Text>
+          <Text className="text-black font-bold mb-3">
+            $38.64
+          </Text>
+
+          {/* Total */}
+          <Text className="text-gray-400 text-sm">Total</Text>
+          <Text className="text-black font-bold mb-3">
+            $199
+          </Text>
+
+          
+
+          {/* Summary */}
+          <Text className="text-gray-400 text-sm">SUBTOTAL</Text>
+          <Text className="text-black font-bold mb-3">
+            $168.64
+          </Text>
+
+          <Text className="text-gray-400 text-sm">TAX TOTAL</Text>
+          <Text className="text-black font-bold mb-3">
+            $30.36
+          </Text>
+
+          <Text className="text-gray-400 text-sm">TOTAL</Text>
+          <Text className="text-black font-bold text-lg mb-4">
+            $199
+          </Text>
+
+        {/* Payment Method */}
+<Text className="text-gray-400 text-sm font-semibold mb-1">
+  Payment Method
+</Text>
+
+<View className="flex-row items-center">
+  {/* Card Type */}
+  <Text className="text-black  text-sm mr-14">
+    Credit Card
+  </Text>
+
+  {/* Mastercard Logo */}
+  <View className="flex-row items-center">
+    {/* Left Circle */}
+    <View
+      className="w-5 h-5 rounded-full"
+      style={{ backgroundColor: "#EB001B", marginRight: -10 }}
+    />
+
+    {/* Right Circle */}
+    <View
+      className="w-5 h-5 rounded-full"
+      style={{ backgroundColor: "#F79E1B" }}
+    />
+  </View>
+
+  {/* Card Digits */}
+  <Text className="text-base font-semibold ml-3">•••• •••• 7917</Text>
+</View>
+
+
+
+          <Text className="text-gray-400 text-sm">Payment Date</Text>
+          <Text className="text-black font-semibold mb-3">
+            10/16/2025, 1:12 AM
+          </Text>
+
+          <Text className="text-gray-400 text-sm">Reference ID</Text>
+          <Text className="text-black font-semibold mb-3">
+            BHM42X90Z03JX
+          </Text>
+
+          <Text className="text-gray-400 text-sm">Place of Supply</Text>
+          <Text className="text-black font-semibold mb-3">
+            Andhra Pradesh
+          </Text>
+
+          <Text className="text-gray-400 text-sm">HSN Code</Text>
+          <Text className="text-black font-semibold mb-3">
+            998439
+          </Text>
+
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
